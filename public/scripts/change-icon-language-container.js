27726ad1fe4b6e101c81d310ec545d69aa9c46fa
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-12-30 18:20:23 -0300

    feat<home>Criação de script com troca de Icone de fundo

---
+++ public/scripts/change-icon-language-container.js
@@ -0,0 +1,8 @@
+function changeIconLanguage(originSrc , originAlt){
+
+    
+    document.getElementById("icon-language").src = originSrc;
+    document.getElementById("icon-language").alt = originAlt;
+  
+}
+
