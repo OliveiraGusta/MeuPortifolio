03f33e45d2c69e89986871ddbe6443c56e7f2c76
OliveiraGusta <gustavo.oliver456@gmail.com>
2024-01-04 01:38:29 -0300

    feat<home>: ajustado icone de fundo da home

--- public/scripts/change-icon-language-container.js
+++ public/scripts/change-icon-language-container.js
@@ -1,8 +1,19 @@
-function changeIconLanguage(originSrc , originAlt){
+function changeIconLanguage(originalSrc, originalAlt) {
+    var iconLanguage = document.getElementById("icon-language");
 
-    
-    document.getElementById("icon-language").src = originSrc;
-    document.getElementById("icon-language").alt = originAlt;
-  
+    var pathArray = (new URL(originalSrc).pathname.split('/')).slice(2);
+    var relativePath = './' + pathArray.join('/');
+
+    var fileName = pathArray[pathArray.length - 1];
+    var modifiedFileName = fileName.replace('.svg', '-lightgreen.svg');
+    var newSrc = relativePath.replace(fileName, modifiedFileName);
+
+    iconLanguage.classList.add("icon-language-transition-class");
+
+    iconLanguage.src = newSrc; 
+    iconLanguage.alt = originalAlt;
+
+    setTimeout(function () {
+        iconLanguage.classList.remove("icon-language-transition-class");
+    }, 1000);
 }
-
